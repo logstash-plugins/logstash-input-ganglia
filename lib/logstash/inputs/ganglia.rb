34f994d13a20533c7852969cc215b82dcb9ece87
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-28 11:10:30 +0200

    clean up stale dependenies

--- lib/logstash/inputs/ganglia.rb
+++ lib/logstash/inputs/ganglia.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 require "date"
-require "logstash/filters/grok"
-require "logstash/filters/date"
 require "logstash/inputs/ganglia/gmondpacket"
 require "logstash/inputs/base"
 require "logstash/namespace"
