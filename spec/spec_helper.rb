8cdb74d0858659268fe116ca6cc8b484b95c31f3
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-10-08 17:44:26 +0200

    cleanup the setup clients helper as is no longer used in the test, we can always reenable it later on when required
    Fixes #13

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,3 @@
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/ganglia"
 require_relative "support/client"
-
-class GangliaHelpers
-
-  def setup_clients(number_of_clients, port)
-    number_of_clients.times.inject([]) do |clients|
-      clients << GangliaClient.new(localhost, port)
-    end
-  end
-
-end
